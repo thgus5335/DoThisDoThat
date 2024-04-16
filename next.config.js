bf56e82ac4c40400abcd66a9a42294288d12a3c7
thgus5335 <thgus5335@naver.com>
2024-04-16 14:27:41 +0900

    Init: 개발 환경 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'assets/styles')],
+    prependData: "@import '@/styles/color.scss'; @import '@/styles/mixin.scss';",
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <p className={styles.test}>main</p>
+    </>
+  );
+}
