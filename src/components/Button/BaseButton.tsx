67f9f19636e58674219111588985b9ba5257d6f8
민영 <minyoung_kim@gimmin-yeong-ui-MacBookAir.local>
2024-04-18 01:51:37 +0900

    feat: DashboardButton 컴포넌트 구현

--- src/components/Button/BaseButton.tsx
+++ src/components/Button/BaseButton.tsx
@@ -1,13 +1,13 @@
 import styles from './BaseButton.module.scss';
 
-interface ButtonProps {
+interface BaseButtonProps {
   children: string;
-  size: string;
+  size: 'large' | 'medium';
   isDisabled?: boolean;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const BaseButton = ({ children, size, isDisabled, onClick }: ButtonProps) => {
+const BaseButton = ({ children, size, isDisabled, onClick }: BaseButtonProps) => {
   return (
     <button className={styles[size]} disabled={isDisabled} onClick={onClick}>
       {children}
--- src/components/Button/TaskButton.tsx
+++ src/components/Button/TaskButton.tsx
@@ -1,13 +1,13 @@
 import styles from './TaskButton.module.scss';
 
-interface ButtonProps {
+interface TaskButtonProps {
   children: string;
-  size: string;
-  color: string;
+  size: 'large' | 'medium' | 'long' | 'small';
+  color: 'violet' | 'white';
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const TaskButton = ({ children, size, color, onClick }: ButtonProps) => {
+const TaskButton = ({ children, size, color, onClick }: TaskButtonProps) => {
   return (
     <button className={`${styles[size]} ${styles[color]}`} onClick={onClick}>
       {children}
