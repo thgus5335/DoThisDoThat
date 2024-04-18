66d8c5e4a5aab9b2b0de6bc1e2ce60b7c8f39d79
seolsis <seoljw1005@naver.com>
2024-04-18 23:11:48 +0900

    feat: 로그인 api 연동

--- src/apis/authService.tsx
+++ src/apis/authService.tsx
@@ -7,6 +7,12 @@ interface UserData {
   password: string;
 }
 
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+// 회원가입
 export const registerUser = async (userData: UserData) => {
   try {
     const response = await httpClient.post('/users', userData);
@@ -17,6 +23,20 @@ export const registerUser = async (userData: UserData) => {
       throw typedError.response.data; // 오류 응답 바디 접근
     } else {
       throw new Error('An unknown error occurred');
+    }
+  }
+};
+
+export const login = async (credentials: Credentials) => {
+  try {
+    const response = await httpClient.post('/auth/login', credentials);
+    return response.data;
+  } catch (error) {
+    const typedError = error as AxiosError;
+    if (typedError.response) {
+      throw typedError.response.data || 'Login failed';
+    } else {
+      throw new Error('Network error');
     }
   }
 };
