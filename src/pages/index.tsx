a35d9e85b72f168a4eecb49a40d11ddde906c341
민영 <minyoung_kim@gimmin-yeong-ui-MacBookAir.local>
2024-04-19 16:27:12 +0900

    feat: MainHeader 컴포넌트 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import MainHeader from '../components/common/Header/MainHeader';
 import styles from './index.module.scss';
 
 export default function Home() {
   return (
     <>
+      <MainHeader />
       <p className={styles.test}>main</p>
     </>
   );
