3747eb32607d3abe47926b638cd401a17b940b3c
thgus5335 <thgus5335@naver.com>
2024-04-25 17:58:41 +0900

    Style: CardList UI 구현

---
+++ src/components/Dashboard/CardList.tsx
@@ -0,0 +1,11 @@
+import styles from './CardList.module.scss';
+
+const CardList = () => {
+  return (
+    <div className={styles.cardList}>
+      <p>하잉</p>
+    </div>
+  );
+};
+
+export default CardList;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,16 +5,48 @@ import { ReactElement } from 'react';
 import styles from './Dashboard.module.scss';
 import HeaderSidebarLayout from '@/src/components/common/Layout/HeaderSidebarLayout';
 import DashboardButton from '@/src/components/common/Button/DashboardButton';
-// import iconCrown from '@/src/assets/icons/crownIcon.svg';
-// import iconAdd from '@/src/assets/icons/addBox.svg';
-// import iconSetting from '@/src/assets/icons/setting.svg';
+import CardList from '@/src/components/Dashboard/CardList';
+import iconSetting from '@/src/assets/icons/setting.svg';
 
 const Dashboard: NextPageWithLayout = () => {
   return (
     <div className={styles.dashboard}>
-      <div className={styles.column}>To Do</div>
-      <div className={styles.column}>On Progress</div>
-      <div className={styles.column}>Done</div>
+      <div className={styles.column}>
+        <div className={styles.columnInfo}>
+          <div className={styles.columnTitle}>
+            <div className={styles.color} />
+            <p>To do</p>
+            <div className={styles.number}>N</div>
+          </div>
+          <Image src={iconSetting} alt="컬럼 환경설정." />
+        </div>
+        <DashboardButton type={'taskLarge'} />
+        <CardList></CardList>
+      </div>
+      <div className={styles.column}>
+        <div className={styles.columnInfo}>
+          <div className={styles.columnTitle}>
+            <div className={styles.color} />
+            <p>On Progress</p>
+            <div className={styles.number}>N</div>
+          </div>
+          <Image src={iconSetting} alt="컬럼 환경설정." />
+        </div>
+        <DashboardButton type={'taskLarge'} />
+        <CardList></CardList>
+      </div>
+      <div className={styles.column}>
+        <div className={styles.columnInfo}>
+          <div className={styles.columnTitle}>
+            <div className={styles.color} />
+            <p>Done</p>
+            <div className={styles.number}>N</div>
+          </div>
+          <Image src={iconSetting} alt="컬럼 환경설정." />
+        </div>
+        <DashboardButton type={'taskLarge'} />
+        <CardList></CardList>
+      </div>
       <div className={styles.addColumn}>
         <DashboardButton type={'columnLarge'}>새로운 컬럼 추가하기</DashboardButton>
       </div>
